bdcad3238dc892a34245f4c5985783b8aae5de14
NIL <nlaurent@octo.com>
2018-07-16 21:34:35 +0200

    refactored calculator and added tests

--- __tests__/Calculator-test.js
+++ __tests__/Calculator-test.js
@@ -1,83 +1,109 @@
 // jest matchers https://jestjs.io/docs/en/using-matchers
-import { computeAttackDeltaPoints, attackWins, individualGamePoints } from '../src/business';
+import { Calculator } from '../src/business';
 import { GAME_STYLES, CONTRACTS, STANDARD_CONTRACTS_POINTS } from '../src/business/constants';
 
+const contractPointValues = STANDARD_CONTRACTS_POINTS;
+const calculator = new Calculator(contractPointValues);
 
 test('delta with zero oudler calculation is correct', () => {
-  deltaScore = computeAttackDeltaPoints(40, 0);
+  deltaScore = calculator.computeAttackDeltaPoints(40, 0);
   expect(deltaScore).toBe(-16)
 });
 
 test('delta with one oudler calculation is correct', () => {
-  deltaScore = computeAttackDeltaPoints(40, 1);
+  deltaScore = calculator.computeAttackDeltaPoints(40, 1);
   expect(deltaScore).toBe(-11)
 });
 
 test('delta with two oudlers calculation is correct', () => {
-  deltaScore = computeAttackDeltaPoints(40, 2);
+  deltaScore = calculator.computeAttackDeltaPoints(40, 2);
   expect(deltaScore).toBe(-1)
 });
 
 test('delta with three oudlers calculation is correct', () => {
-  deltaScore = computeAttackDeltaPoints(40, 3);
+  deltaScore = calculator.computeAttackDeltaPoints(40, 3);
   expect(deltaScore).toBe(4)
 });
 
 test('delta with incorrect oudlers calculation throws exception', () => {
   expect(() => {
-    computeAttackDeltaPoints(40, 5)
+    calculator.computeAttackDeltaPoints(40, 5)
   }).toThrow('incorrect oudler count, should be 0, 1, 2 or 3')
 });
 
 test('attack loses', () => {
-  expect(attackWins(55, 0)).toBeFalsy();
-  expect(attackWins(50, 1)).toBeFalsy();
-  expect(attackWins(40, 2)).toBeFalsy();
-  expect(attackWins(35, 3)).toBeFalsy();
+  expect(calculator.attackWins(55, 0)).toBeFalsy();
+  expect(calculator.attackWins(50, 1)).toBeFalsy();
+  expect(calculator.attackWins(40, 2)).toBeFalsy();
+  expect(calculator.attackWins(35, 3)).toBeFalsy();
 });
 
 test('attack wins with attack score striclty higher than oudler count points', () => {
-  expect(attackWins(58, 0)).toBeTruthy();
-  expect(attackWins(53, 1)).toBeTruthy();
-  expect(attackWins(45, 2)).toBeTruthy();
-  expect(attackWins(40, 3)).toBeTruthy();
+  expect(calculator.attackWins(58, 0)).toBeTruthy();
+  expect(calculator.attackWins(53, 1)).toBeTruthy();
+  expect(calculator.attackWins(45, 2)).toBeTruthy();
+  expect(calculator.attackWins(40, 3)).toBeTruthy();
 });
 
 test('attack wins with attack score equals to oudler count points', () => {
-  expect(attackWins(56, 0)).toBeTruthy();
-  expect(attackWins(51, 1)).toBeTruthy();
-  expect(attackWins(41, 2)).toBeTruthy();
-  expect(attackWins(36, 3)).toBeTruthy();
+  expect(calculator.attackWins(56, 0)).toBeTruthy();
+  expect(calculator.attackWins(51, 1)).toBeTruthy();
+  expect(calculator.attackWins(41, 2)).toBeTruthy();
+  expect(calculator.attackWins(36, 3)).toBeTruthy();
 });
 
-test('attack point calculation is correct for 3 game style if attack wins a prise', () => {
-  const contractPointValues = STANDARD_CONTRACTS_POINTS;
-  expect(individualGamePoints(36, 3, CONTRACTS.prise, contractPointValues)).toBe(contractPointValues.prise);
-  expect(individualGamePoints(41, 2, CONTRACTS.prise, contractPointValues)).toBe(contractPointValues.prise);
-  expect(individualGamePoints(51, 1, CONTRACTS.prise, contractPointValues)).toBe(contractPointValues.prise);
-  expect(individualGamePoints(56, 0, CONTRACTS.prise, contractPointValues)).toBe(contractPointValues.prise);
+test('attack point calculation is correct if attack wins a prise', () => {
+  expect(calculator.computeIndividualGamePoints(36, 3, CONTRACTS.prise)).toBe(contractPointValues.prise);
+  expect(calculator.computeIndividualGamePoints(41, 2, CONTRACTS.prise)).toBe(contractPointValues.prise);
+  expect(calculator.computeIndividualGamePoints(51, 1, CONTRACTS.prise)).toBe(contractPointValues.prise);
+  expect(calculator.computeIndividualGamePoints(56, 0, CONTRACTS.prise)).toBe(contractPointValues.prise);
 });
 
-test('attack point calculation is correct for 3 game style if attack wins a garde', () => {
-  const contractPointValues = STANDARD_CONTRACTS_POINTS;
-  expect(individualGamePoints(36, 3, CONTRACTS.garde, contractPointValues)).toBe(contractPointValues.garde);
-  expect(individualGamePoints(41, 2, CONTRACTS.garde, contractPointValues)).toBe(contractPointValues.garde);
-  expect(individualGamePoints(51, 1, CONTRACTS.garde, contractPointValues)).toBe(contractPointValues.garde);
-  expect(individualGamePoints(56, 0, CONTRACTS.garde, contractPointValues)).toBe(contractPointValues.garde);
+test('attack point calculation is correct if attack loses a prise', () => {
+  expect(calculator.computeIndividualGamePoints(35, 3, CONTRACTS.prise)).toBe(-(contractPointValues.prise + 1));
+  expect(calculator.computeIndividualGamePoints(39, 2, CONTRACTS.prise)).toBe(-(contractPointValues.prise + 2));
+  expect(calculator.computeIndividualGamePoints(45, 1, CONTRACTS.prise)).toBe(-(contractPointValues.prise + 6));
+  expect(calculator.computeIndividualGamePoints(45, 0, CONTRACTS.prise)).toBe(-(contractPointValues.prise + 11));
 });
 
-test('attack point calculation is correct for 3 game style if attack wins a garde sans', () => {
-  const contractPointValues = STANDARD_CONTRACTS_POINTS;
-  expect(individualGamePoints(36, 3, CONTRACTS.gardeSans, contractPointValues)).toBe(contractPointValues.gardeSans);
-  expect(individualGamePoints(41, 2, CONTRACTS.gardeSans, contractPointValues)).toBe(contractPointValues.gardeSans);
-  expect(individualGamePoints(51, 1, CONTRACTS.gardeSans, contractPointValues)).toBe(contractPointValues.gardeSans);
-  expect(individualGamePoints(56, 0, CONTRACTS.gardeSans, contractPointValues)).toBe(contractPointValues.gardeSans);
+test('attack point calculation is correct if attack wins a garde', () => {
+  expect(calculator.computeIndividualGamePoints(36, 3, CONTRACTS.garde)).toBe(contractPointValues.garde);
+  expect(calculator.computeIndividualGamePoints(41, 2, CONTRACTS.garde)).toBe(contractPointValues.garde);
+  expect(calculator.computeIndividualGamePoints(51, 1, CONTRACTS.garde)).toBe(contractPointValues.garde);
+  expect(calculator.computeIndividualGamePoints(56, 0, CONTRACTS.garde)).toBe(contractPointValues.garde);
 });
 
-test('attack point calculation is correct for 3 game style if attack wins a garde contre', () => {
-  const contractPointValues = STANDARD_CONTRACTS_POINTS;
-  expect(individualGamePoints(36, 3, CONTRACTS.gardeContre, contractPointValues)).toBe(contractPointValues.gardeContre);
-  expect(individualGamePoints(41, 2, CONTRACTS.gardeContre, contractPointValues)).toBe(contractPointValues.gardeContre);
-  expect(individualGamePoints(51, 1, CONTRACTS.gardeContre, contractPointValues)).toBe(contractPointValues.gardeContre);
-  expect(individualGamePoints(56, 0, CONTRACTS.gardeContre, contractPointValues)).toBe(contractPointValues.gardeContre);
+test('attack point calculation is correct if attack loses a garde', () => {
+  expect(calculator.computeIndividualGamePoints(35, 3, CONTRACTS.garde)).toBe(-(contractPointValues.garde + 1));
+  expect(calculator.computeIndividualGamePoints(39, 2, CONTRACTS.garde)).toBe(-(contractPointValues.garde + 2));
+  expect(calculator.computeIndividualGamePoints(45, 1, CONTRACTS.garde)).toBe(-(contractPointValues.garde + 6));
+  expect(calculator.computeIndividualGamePoints(45, 0, CONTRACTS.garde)).toBe(-(contractPointValues.garde + 11));
+});
+
+test('attack point calculation is correct if attack wins a garde sans', () => {
+  expect(calculator.computeIndividualGamePoints(36, 3, CONTRACTS.gardeSans)).toBe(contractPointValues.gardeSans);
+  expect(calculator.computeIndividualGamePoints(41, 2, CONTRACTS.gardeSans)).toBe(contractPointValues.gardeSans);
+  expect(calculator.computeIndividualGamePoints(51, 1, CONTRACTS.gardeSans)).toBe(contractPointValues.gardeSans);
+  expect(calculator.computeIndividualGamePoints(56, 0, CONTRACTS.gardeSans)).toBe(contractPointValues.gardeSans);
+});
+
+test('attack point calculation is correct if attack loses a garde sans', () => {
+  expect(calculator.computeIndividualGamePoints(35, 3, CONTRACTS.gardeSans)).toBe(-(contractPointValues.gardeSans + 1));
+  expect(calculator.computeIndividualGamePoints(39, 2, CONTRACTS.gardeSans)).toBe(-(contractPointValues.gardeSans + 2));
+  expect(calculator.computeIndividualGamePoints(45, 1, CONTRACTS.gardeSans)).toBe(-(contractPointValues.gardeSans + 6));
+  expect(calculator.computeIndividualGamePoints(45, 0, CONTRACTS.gardeSans)).toBe(-(contractPointValues.gardeSans + 11));
+});
+
+test('attack point calculation is correct if attack wins a garde contre', () => {
+  expect(calculator.computeIndividualGamePoints(36, 3, CONTRACTS.gardeContre)).toBe(contractPointValues.gardeContre);
+  expect(calculator.computeIndividualGamePoints(41, 2, CONTRACTS.gardeContre)).toBe(contractPointValues.gardeContre);
+  expect(calculator.computeIndividualGamePoints(51, 1, CONTRACTS.gardeContre)).toBe(contractPointValues.gardeContre);
+  expect(calculator.computeIndividualGamePoints(56, 0, CONTRACTS.gardeContre)).toBe(contractPointValues.gardeContre);
+});
+
+test('attack point calculation is correct if attack loses a garde contre', () => {
+  expect(calculator.computeIndividualGamePoints(35, 3, CONTRACTS.gardeContre)).toBe(-(contractPointValues.gardeContre + 1));
+  expect(calculator.computeIndividualGamePoints(39, 2, CONTRACTS.gardeContre)).toBe(-(contractPointValues.gardeContre + 2));
+  expect(calculator.computeIndividualGamePoints(45, 1, CONTRACTS.gardeContre)).toBe(-(contractPointValues.gardeContre + 6));
+  expect(calculator.computeIndividualGamePoints(45, 0, CONTRACTS.gardeContre)).toBe(-(contractPointValues.gardeContre + 11));
 });
--- src/business/Calculator.js
+++ src/business/Calculator.js
@@ -2,68 +2,82 @@
 import { IllegalArgumentError } from './IllegalArgumentError';
 import { OUDLER_COUNT_POINTS, CONTRACTS } from './constants';
 
-export const computeAttackDeltaPoints = (attackScore, attackOudlerCount) => {
-  let targetScore = 0;
-  switch (attackOudlerCount) {
-    case 0:
-      targetScore = OUDLER_COUNT_POINTS.zeroOudler;
-      break;
-    case 1:
-      targetScore = OUDLER_COUNT_POINTS.oneOudler;
-      break;
-    case 2:
-      targetScore = OUDLER_COUNT_POINTS.twoOudlers;
-      break;
-    case 3:
-      targetScore = OUDLER_COUNT_POINTS.threeOudlers;
-      break;
-    default:
+export class Calculator {
+  constructor(contractPointValues) {
+    this.contractPointValues = contractPointValues;
+  }
+
+  computeAttackDeltaPoints = (attackScore, attackOudlerCount) => {
+    let targetScore = 0;
+    switch (attackOudlerCount) {
+      case 0:
+        targetScore = OUDLER_COUNT_POINTS.zeroOudler;
+        break;
+      case 1:
+        targetScore = OUDLER_COUNT_POINTS.oneOudler;
+        break;
+      case 2:
+        targetScore = OUDLER_COUNT_POINTS.twoOudlers;
+        break;
+      case 3:
+        targetScore = OUDLER_COUNT_POINTS.threeOudlers;
+        break;
+      default:
+        throw new IllegalArgumentError(
+          'incorrect oudler count, should be 0, 1, 2 or 3'
+        );
+    }
+    return attackScore - targetScore;
+  };
+
+  attackWins = (attackScore, attackOudlerCount) => {
+    switch (attackOudlerCount) {
+      case 0:
+        return attackScore >= OUDLER_COUNT_POINTS.zeroOudler;
+      case 1:
+        return attackScore >= OUDLER_COUNT_POINTS.oneOudler;
+      case 2:
+        return attackScore >= OUDLER_COUNT_POINTS.twoOudlers;
+      case 3:
+        return attackScore >= OUDLER_COUNT_POINTS.threeOudlers;
+      default:
+        throw new IllegalArgumentError(
+          'incorrect oudler count, should be 0, 1, 2 or 3'
+        );
+    }
+  };
+
+  computeIndividualGamePoints = (attackScore, attackOudlerCount, contract) => {
+    let contractPoints = 0;
+    switch (contract) {
+      case CONTRACTS.prise:
+        contractPoints = this.contractPointValues.prise;
+        break;
+      case CONTRACTS.garde:
+        contractPoints = this.contractPointValues.garde;
+        break;
+      case CONTRACTS.gardeSans:
+        contractPoints = this.contractPointValues.gardeSans;
+        break;
+      case CONTRACTS.gardeContre:
+        contractPoints = this.contractPointValues.gardeContre;
+        break;
+      default:
       throw new IllegalArgumentError(
-        'incorrect oudler count, should be 0, 1, 2 or 3'
+        'incorrect contract, should be prise, garde, gardeSans or gardeContre'
       );
-  }
+    }
 
-  return attackScore - targetScore;
-};
+    const attackDeltaPoints = this.computeAttackDeltaPoints(attackScore, attackOudlerCount);
+    const attackWins = this.attackWins(attackScore, attackOudlerCount);
+    const absoluteIndividualPlayerPoints = contractPoints + Math.abs(attackDeltaPoints);
+    const individualPlayerPoints = attackWins ? absoluteIndividualPlayerPoints : -1 * absoluteIndividualPlayerPoints;
 
-export const attackWins = (attackScore, attackOudlerCount) => {
-  switch (attackOudlerCount) {
-    case 0:
-      return attackScore >= OUDLER_COUNT_POINTS.zeroOudler;
-    case 1:
-      return attackScore >= OUDLER_COUNT_POINTS.oneOudler;
-    case 2:
-      return attackScore >= OUDLER_COUNT_POINTS.twoOudlers;
-    case 3:
-      return attackScore >= OUDLER_COUNT_POINTS.threeOudlers;
-    default:
-      throw new IllegalArgumentError(
-        'incorrect oudler count, should be 0, 1, 2 or 3'
-      );
-  }
-};
+    // console.log('attackDeltaPoints ' + attackDeltaPoints);
+    // console.log('attackWins ' + attackWins);
+    // console.log('absoluteIndividualPlayerPoints ' + absoluteIndividualPlayerPoints);
+    // console.log('individualPlayerPoints ' + individualPlayerPoints);
 
-export const individualGamePoints =
-  (attackScore, attackOudlerCount, contract, contractPointValues) => {
-  let contractPoints = 0;
-  switch (contract) {
-    case CONTRACTS.prise:
-      contractPoints = contractPointValues.prise;
-      break;
-    case CONTRACTS.garde:
-      contractPoints = contractPointValues.garde;
-      break;
-    case CONTRACTS.gardeSans:
-      contractPoints = contractPointValues.gardeSans;
-      break;
-    case CONTRACTS.gardeContre:
-      contractPoints = contractPointValues.gardeContre;
-      break;
-    default:
-  }
-
-  const individualPlayerPoints =
-    contractPoints + computeAttackDeltaPoints(attackScore, attackOudlerCount);
-
-  return individualPlayerPoints;
-};
+    return individualPlayerPoints;
+  };
+}
